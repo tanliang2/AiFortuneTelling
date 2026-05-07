0ad747f47555e33c005206c4bc08ad864f835cda
tanliang <tanliang@300624.cn>
2026-05-07 11:05:25 +0800

    Initial Commit

---
+++ AiFortuneTellingTests/AiFortuneTellingTests.swift
@@ -0,0 +1,19 @@
+//
+//  AiFortuneTellingTests.swift
+//  AiFortuneTellingTests
+//
+//  Created by ws on 2026/5/7.
+//
+
+import Testing
+@testable import AiFortuneTelling
+
+struct AiFortuneTellingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
